f92fdf0b3995adcac5a40910ad002ab43ea37311
ZJH <zjh@zjh.com>
2020-12-18 19:51:44 +0800

    hello world3 by zjh

--- src/main/java/com/rzz/demo/zjh/HelloWorld.java
+++ src/main/java/com/rzz/demo/zjh/HelloWorld.java
@@ -3,5 +3,6 @@ package com.rzz.demo.zjh;
 public class HelloWorld {
     public static void hello(){
         System.out.println("Hello World!!!");
+        System.out.println("hello world 3");
     }
 }
