86828a2ed9264569794873c4d9c5d8cadde0382b
rxm_glb <ruanxingming@qq.com>
2020-12-19 11:38:50 +0800

    初始化环境搭建

---
+++ yeb-server/src/main/java/com/xxxx/server/mapper/AdminMapper.java
@@ -0,0 +1,16 @@
+package com.xxxx.server.mapper;
+
+import com.xxxx.server.pojo.Admin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author rzz
+ * @since 2020-12-19
+ */
+public interface AdminMapper extends BaseMapper<Admin> {
+
+}
---
+++ yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
@@ -0,0 +1,16 @@
+package com.xxxx.server.service;
+
+import com.xxxx.server.pojo.Admin;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author rzz
+ * @since 2020-12-19
+ */
+public interface IAdminService extends IService<Admin> {
+
+}
